e07e1a32cfba7c1eb3fd30e0e390d7b37eb3cf6f
hyeok jin son <hjson817@gmail.com>
2020-11-12 18:35:54 +0900

    html전체변경

--- photo/views.py
+++ photo/views.py
@@ -3,19 +3,28 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import  CreateView,UpdateView,DeleteView
 from django.views.generic.detail import DetailView
 from .models import Photo
+from django.shortcuts import redirect
 class PhotoList(ListView):
     model=Photo
     template_name_suffix = '_list'
 
 class PhotoCreate(CreateView):
     model = Photo
-    fields = ['text','image']
+    fields = ['author','text','image']
     template_name_suffix = '_create'
     success_url = '/'
 
+    def form_invalid(self, form):
+        form.instance.author_id=self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form':form})
+
 class PhotoUpdate(UpdateView):
     model = Photo
-    fields = ['text','image']
+    fields = ['author','text','image']
     template_name_suffix = '_update'
     success_url = '/'
 
